8b022cb0b8e13fad42ded1b1caf5af50af6371ab
焦佳男1 <1@DESKTOP-BGU1C30>
2016-11-28 11:47:05 +0800

    解决冲突

--- xnradmin-model/xnradmin-client/src/main/java/com/xnradmin/client/service/IndexFrontService.java
+++ xnradmin-model/xnradmin-client/src/main/java/com/xnradmin/client/service/IndexFrontService.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cntinker.util.StringHelper;
-import com.sun.webkit.dom.RectImpl;
 import com.xnradmin.core.dao.CommonDAO;
 import com.xnradmin.po.business.BusinessCategory;
 import com.xnradmin.po.business.BusinessGoods;
