afbe4d07626aa634773a037e82520581bcdc8614
caoyichao <879854705@qq.com>
2016-12-01 09:39:40 +0800

    农户订单表

---
+++ xnradmin-model/xnradmin-data/src/main/java/com/xnradmin/po/business/FarmerOrderRecord.java
@@ -0,0 +1,124 @@
+package com.xnradmin.po.business;
+
+
+
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "farmer_order_record")
+public class FarmerOrderRecord implements java.io.Serializable{
+
+    // Fields
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+
+	private Long orderRecordId; //订单ID
+    
+	private Long outPlanId; //生成计划id
+	
+	private Long farmerId;    //农户id
+	
+	private Integer goodsId; //商品Id
+    
+    private Integer goodsCount; //商品数量
+      
+    private String staffId;	//分配订单id
+    
+    private Timestamp createTime; //生成时间 
+    
+    
+    /** default constructor */
+    public FarmerOrderRecord(){
+    }
+
+    // Property accessors
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "ID", unique = true, nullable = false)
+    public Integer getId(){
+        return this.id;
+    }
+
+    public void setId(Integer id){
+        this.id = id;
+    }
+
+    @Column(name = "ORDER_RECORD_ID" ,length=20)
+	public Long getOrderRecordId() {
+		return orderRecordId;
+	}
+
+	public void setOrderRecordId(Long orderRecordId) {
+		this.orderRecordId = orderRecordId;
+	}
+
+	@Column(name = "GOODS_COUNT")
+	public Integer getGoodsCount() {
+		return goodsCount;
+	}
+
+	public void setGoodsCount(Integer goodsCount) {
+		this.goodsCount = goodsCount;
+	}
+
+	@Column(name = "GOODS_ID")
+	public Integer getGoodsId() {
+		return goodsId;
+	}
+
+	public void setGoodsId(Integer goodsId) {
+		this.goodsId = goodsId;
+	}
+	
+	@Column(name = "STAFF_ID")
+	public String getStaffId() {
+		return staffId;
+	}
+
+	public void setStaffId(String staffId) {
+		this.staffId = staffId;
+	}
+
+	public Long getFarmerId() {
+		return farmerId;
+	}
+
+	public void setFarmerId(Long farmerId) {
+		this.farmerId = farmerId;
+	}
+
+	public Timestamp getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Timestamp createTime) {
+		this.createTime = createTime;
+	}
+
+	public Long getOutPlanId() {
+		return outPlanId;
+	}
+
+	public void setOutPlanId(Long outPlanId) {
+		this.outPlanId = outPlanId;
+	}
+
+	
+	
+}
